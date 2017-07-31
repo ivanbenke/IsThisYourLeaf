d11b63e0414e736bd0b68b409abf33a0f04d5a70
tsarcevic <tsarcevic@etfos.hr>
2017-07-31 20:32:23 +0200

    database updated, changed result page

--- index.php
+++ index.php
@@ -76,9 +76,9 @@ if (isset($properties)) {
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<title>RUAP</title>
 </head>
 <body>
+<h1>What is my leaf?</h1>
 <div class="container">
 <form action="" method="POST">
 <div class="form-group">
