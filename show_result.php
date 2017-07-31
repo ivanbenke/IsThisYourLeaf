697dd3a234a7d8b6a80d30c0b9de176d48e8332c
ivanbenke <benke.ivan@gmail.com>
2017-07-31 16:44:34 +0200

    connect and read from db added

--- show_result.php
+++ show_result.php
@@ -1,23 +1,41 @@
-<?php session_start();
+<?php
+session_start();
+
+$servername = "localhost";
+$user = "admin";
+$pass = "admin";
+$database = "ruap";
+
+$conn = new mysqli($servername, $user, $pass, $database);
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+
 if (isset($_SESSION["class"]) && isset($_SESSION["probability"])) {
 	$class = $_SESSION["class"];
 	$probability = $_SESSION["probability"];
+	$sql = "SELECT name FROM leaf WHERE class='$class'";
+	$res = $conn->query($sql);
+	$row = $res->fetch_assoc();
+	$name = $row["name"];
 	session_unset();
 } else {
 	header("Location: index.php");
 }
 ?>
 <!DOCTYPE html>
-<html>
 <head>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <title>RUAP</title>
+</script>
 </head>
 <body>
 <div class="container">
-	<p><?php echo $probability; ?>% da je <?php echo $class; ?>. klasa.</p>
+	<p><?php echo $probability; ?>% da je <?php echo $class; ?>. klasa, tj. <?php echo $name; ?>.</p>
+</div>
 </div>
 </body>
 </html>
